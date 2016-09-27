f0ca8db01990f323f531bb0b44b10e680c46a96e
madmann <madmann686@Gmail.com>
2016-09-27 12:13:45 -0400

    Final, No Errors, Switched to Arrays :(

---
+++ src/Cards/Card.java
@@ -0,0 +1,27 @@
+package Cards;
+
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Card {
+    private String[] suits={"Clubs","Hearts","Spades","Diamonds"};
+    private String[] values={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace"};
+
+    private String mSuit;
+    private String mValue;
+    public Card(){
+        genCard();
+    }
+
+    private void genCard(){
+        Random random=new Random();
+        mSuit=suits[random.nextInt(suits.length)];
+        mValue=values[random.nextInt(values.length)];
+    }
+
+    @Override
+    public String toString() {
+        return (mValue+" of "+mSuit);
+    }
+}
---
+++ src/Cards/Main.java
@@ -0,0 +1,13 @@
+package Cards;
+
+/**
+ * Created by madmann on 9/27/16.
+ */
+public class Main {
+    public static void main(String[] args){
+        for (int i=0;i<20;i++){
+            Card card=new Card();
+            System.out.println(card.toString());
+        }
+    }
+}
