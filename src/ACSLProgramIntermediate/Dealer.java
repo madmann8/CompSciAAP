bb6ff3bad20af830d885be40dd21a00b2f3ab101
madmann <madmann686@Gmail.com>
2016-12-14 16:13:03 -0500

    Final

--- src/ACSLProgramIntermediate/Dealer.java
+++ src/ACSLProgramIntermediate/Dealer.java
@@ -92,36 +92,6 @@ public class Dealer {
                     return null;
             }
         }
-//                    switch (in) {
-//            case "A":
-//                return ACE;
-//            case "2":
-//                return TWO;
-//            case "3":
-//                return THREE;
-//            case "4":
-//                return FOUR;
-//            case "5":
-//                return FIVE;
-//            case "6":
-//                return SIX;
-//            case "7":
-//                return SEVEN;
-//            case "8":
-//                return EIGHT;
-//            case "9":
-//                return NINE;
-//            case "T":
-//                return TEN;
-//            case "J":
-//                return JACK;
-//            case "Q":
-//                return QUEEN;
-//            case "K":
-//                return KING;
-//            default:
-//                return null;
-//        }
 
     }
 
@@ -136,19 +106,7 @@ public class Dealer {
                     return false;
                 }
             }
-//            if (value1.rank < faceUpValue.rank && value2.rank < faceUpValue.rank) {
-//                if (value1.rank < value2.rank) {
-//                    return true;
-//                } else {
-//                    return false;
-//                }
-//            }
-//            if (value1.rank >= faceUpValue.rank && value2.rank> faceUpValue.rank && value1.rank < value2.rank) {
-//                return true;
-//            }
-//            if (value2.rank >= faceUpValue.rank && value1.rank> faceUpValue.rank && value2.rank < value1.rank) {
-//                return false;
-//            }
+
             else {
                 if (value1.rank < value2.rank) {
                     return true;
