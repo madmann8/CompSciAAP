65b9ef51735f6cce9891bc1b5bd4236723f3bd4d
madmann <madmann686@Gmail.com>
2017-02-08 18:37:29 -0500

    Final

--- src/HassaanStuff/ACSLNumeroDose.java
+++ src/HassaanStuff/ACSLNumeroDose.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class ACSLNumeroDose {
     public static void main(String[] args) {
-        System.out.println("HAAASSSSAAANNN Custom Text:");
-        Scanner scanner = new Scanner(System.in);
-        String original = scanner.next();
-        scanner.close();
-        int[] printArray = new int[original.length()];
         int num1 = 1;
         int num2 = 0;
         String pos1;
+        System.out.println("HAAASSSSAAANNN Custom Tet:");
+        Scanner scanner = new Scanner(System.in);
+        int[] printArray = new int[original.length()];
+        String original = scanner.next();
+        scanner.close();
         pos1 = String.valueOf(original.charAt(0));
         printArray[0] = Integer.valueOf(original.substring(1, 1 + Integer.valueOf(pos1)));
         for (int i = original.length() - 1; i > 1; i--) {
@@ -22,8 +22,12 @@ public class ACSLNumeroDose {
                 }
             }
             String next = "";
-            if (i - num2 >= 1) next = String.valueOf(original.charAt(i - num2));
-            else next = String.valueOf(original.charAt(i));
+            if (i - num2 >= 1) {
+                next = String.valueOf(original.charAt(i - num2));
+            }
+            else {
+                next = String.valueOf(original.charAt(i));
+            }
             while (compare >= Integer.valueOf(next)) {
                 if (i - num1 <= 1) {
                     break;
@@ -37,10 +41,11 @@ public class ACSLNumeroDose {
                 printArray[printArray.length - i] = Integer.valueOf(next);
             }
         }
-        for (int num = 0; num < printArray.length; num++)
-            if (num != 0) {
-                System.out.print(num);
+        for (int k:printArray) {
+            if (k != 0) {
+                System.out.print(k);
                 System.out.print(" ");
             }
+        }
     }
 }
